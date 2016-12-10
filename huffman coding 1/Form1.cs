a3981be1cc6afefef7405e464beb48e83c2bf32c
MohabMohamed <mohab.m.mohamed@gmail.com>
2016-12-10 16:12:41 +0200

    created huffman compression

---
+++ huffman coding 1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace huffman_coding_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter wr = new StreamWriter("message.txt");
+            wr.WriteLine(textBox1.Text);
+            wr.Close();
+            
+            string input = File.ReadAllText("message.txt").Replace("\r\n","");
+
+            HuffmanTree tree = new HuffmanTree();
+
+            tree.setDict(input);
+
+            tree.setnodelist(tree.Dict);
+
+            tree.setHuffmanCode(tree.nodelist[0], ""); //root node
+
+
+            StreamWriter wr3 = new StreamWriter("code.txt");
+            foreach (KeyValuePair<char, string> entry in tree.HuffmanDict)
+            {
+                wr3.WriteLine(entry);
+            }
+            wr3.Close();
+
+            //generate compressed code
+
+            string compressed = "";
+            foreach (char entry in input)
+            {
+                compressed += tree.HuffmanDict[entry];
+            }
+
+            StreamWriter wr4 = new StreamWriter("compressed.txt");
+            wr4.Write(compressed);
+            wr4.Close();
+
+
+        }
+    }
+}
---
+++ huffman coding 1/Node.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace huffman_coding_1
+{
+    public class Node
+    {
+
+        public char Data { get; set; }
+        public int Frequency { get; set; }
+        public Node Right { get; set; }
+        public Node Left { get; set; }
+        public string huffman_code {get; set;}
+
+        public Node(char data, int freq)
+        {
+            Right = Left = null;
+            this.Data = data;
+            this.Frequency = freq;
+        }
+
+        public Node(Node left, Node right)
+        {
+            this.Left = left;
+            this.Right = right;
+            Frequency = left.Frequency + right.Frequency;
+        }
+
+        public int CompareTo(Node x)
+        {
+            if (this.Frequency > x.Frequency)
+                return 1;
+            else if (this.Frequency < x.Frequency)
+                return -1;
+            else
+                return 0;
+
+        }
+        
+    }
+
+   
+
+}
